acf8fa1d569d3de148c99b62e36c2eaee4e0ada6
Jerry Lundström <lundstrom.jerry@gmail.com>
2016-06-28 15:35:11 +0200

    .

--- autobuild.sh
+++ autobuild.sh
@@ -8,6 +8,7 @@ mkdir -p "$HOME/deb" "$HOME/build"
 find "$HOME/deb" -type f -name '*_source.changes' |
   while read file; do
     test -f "$file.built" && continue
+    test -f "$file.build-err" && continue
     name=`echo "$file" | sed 's%.*/%%' | sed 's%_source.changes$%%'`
     orig=`echo "$name" | sed 's%-[0-9]*~[a-z]*+[0-9]*$%%'`
     pkg=`echo "$orig" | sed 's%_.*%%'`
@@ -19,7 +20,8 @@ find "$HOME/deb" -type f -name '*_source.changes' |
       mkdir -p "$HOME/deb/result" &&
       cp -nv "$HOME/pbuilder/"*"_result/$name"* \
         "$HOME/deb/result/" &&
-      touch "$file.built"
+      touch "$file.built" ||
+      touch "$file.build-err"
       cp -nv "$HOME/pbuilder/"*"_result/$pkg-dbgsym_$vers-"* \
         "$HOME/deb/result/"
     ) >"$HOME/deb/$name.log" 2>&1
