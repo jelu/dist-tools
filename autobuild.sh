c94cf5ce8cec57f9c71b36bda7c205793996576d
Jerry Lundström <lundstrom.jerry@gmail.com>
2016-06-28 14:57:39 +0200

    .

--- autobuild.sh
+++ autobuild.sh
@@ -9,17 +9,19 @@ find "$HOME/deb" -type f -name '*_source.changes' |
   while read file; do
     test -f "$file.built" && continue
     name=`echo "$file" | sed 's%.*/%%' | sed 's%_source.changes$%%'`
-    orig=`echo "$name" | sed 's%-.*%%'`
+    orig=`echo "$name" | sed 's%-[0-9]*~[a-z]*+[0-9]*$%%'`
     pkg=`echo "$orig" | sed 's%_.*%%'`
     vers=`echo "$orig" | sed 's%.*_%%'`
     (
-      cp -v "$HOME/deb/$name"* "$HOME/deb/$orig.orig."* "$HOME/build" &&
+      cp -v "$HOME/deb/$orig.orig."* "$HOME/build"
+      cp -v "$HOME/deb/$name"* "$HOME/build" &&
       ( cd "$HOME/build" && "$HOME/dist-tools/cowbuilder-dist" "${name}_source.changes" ) &&
       mkdir -p "$HOME/deb/result" &&
       cp -nv "$HOME/pbuilder/"*"_result/$name"* \
-        "$HOME/pbuilder/"*"_result/$pkg-dbgsym_$vers-"* \
         "$HOME/deb/result/" &&
       touch "$file.built"
+      cp -nv "$HOME/pbuilder/"*"_result/$pkg-dbgsym_$vers-"* \
+        "$HOME/deb/result/"
     ) >"$HOME/deb/$name.log" 2>&1
   done
 
